b48dea57cb4656cf8c0d526709368ce8daca34a6
Alexander Swann <alex_swann@hotmail.co.uk>
2020-12-06 13:25:34 +0000

    Use shorthand map chars

--- Answers/6.rb
+++ Answers/6.rb
@@ -1,7 +1,7 @@
 arr, total = File.readlines('./Test_Data/6.txt').map(&:strip).join(' ').split('  '), 0
 
 arr.each do |group|
-  group = group.split.map { |answer| answer.chars }
+  group = group.split.map(&:chars)
   common = group.shift
   common &= group.shift while group[0] && common[0]
   total += common.length
